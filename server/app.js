9d27f00e6d3fa0b25d76a53175eb1cb0c8d13cab
Ryan Harrington <rharrington.web@gmail.com>
2020-10-26 13:46:01 -0600

    fix: separated server and client code

--- server/app.js
+++ server/app.js
@@ -8,19 +8,19 @@ const port = 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.resolve('../public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.status(200).sendFile('index.html', {
-        root: path.resolve('public')
+        root: path.resolve('../public')
     });
 });
 
 app.get('/admin', (req, res)  => {
     res.status(200).sendFile('admin.html', {
-        root: path.resolve('public')
+        root: path.resolve('../public')
     });
 })
 
